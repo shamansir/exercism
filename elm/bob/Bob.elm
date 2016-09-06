4f49877e468d4f9e220c09b0f0d5b78453a5803d
Ulric Wilfred <shaman.sir@gmail.com>
2016-09-06 13:48:29 +0200

    elm/bob is solved

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,15 +1,71 @@
 module Bob exposing (..)
 
-type Phrase = Silence | Statement | Shout | Question
+import String
+import Char
+
+type Phrase
+    = NotDetermined
+    | Silence
+    | Statement
+    | Question
+    | Shout
+
+isSpace : Char -> Bool
+isSpace char =
+    (char == ' ') ||
+    (char == '\t') ||
+    (char == '\n')
+
+howMuch : (Char -> Bool) -> String -> Int
+howMuch check str =
+    String.length (String.filter check str)
+
+isShoutingWord : String -> Bool
+isShoutingWord word =
+    (String.all Char.isUpper word) &&
+    not (word == "OK") &&
+    not (word == "DMV")
+
+looksLikeShout : String -> Bool
+looksLikeShout str =
+    if String.endsWith "GO!" str then
+        True
+    else
+        let
+            wordsCount = List.length (String.words str)
+            shoutWordsCount = List.length (List.filter isShoutingWord (String.words str))
+        in
+            (toFloat shoutWordsCount) >= ((toFloat wordsCount) / 2)
 
 determine : String -> Phrase
 determine str =
-    Silence
+    if String.isEmpty str || String.all isSpace str then
+        Silence
+    -- else if (any isUpper str) then
+    else if looksLikeShout str then
+        Shout
+    else if String.endsWith "." str then
+        Statement
+    else if String.endsWith "?" str then
+        Question
+    else
+        Statement -- NotDetermined
 
 response : Phrase -> String
 response phrase =
-    "."
+    case phrase of
+        NotDetermined ->
+            "My bad."
+        Silence ->
+            "Fine. Be that way!"
+        Statement ->
+            "Whatever."
+        Question ->
+            "Sure."
+        Shout ->
+            "Whoa, chill out!"
 
 hey : String -> String
 hey request =
-    response (determine request)
+    determine request
+        |> response
