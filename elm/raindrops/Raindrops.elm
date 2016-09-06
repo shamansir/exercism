34b7d2a3a011aea4c3d317361a051f76977caec6
Ulric Wilfred <shaman.sir@gmail.com>
2016-09-07 01:29:15 +0300

    resolve elm/Raindrops

---
+++ elm/raindrops/Raindrops.elm
@@ -0,0 +1,39 @@
+module Raindrops exposing (..)
+
+import String
+
+factors : List Int
+factors = [ 3, 5, 7 ]
+
+hasFactor : Int -> Int -> Bool
+hasFactor factor num =
+    num % factor == 0
+
+correspondingDrop : Int -> String
+correspondingDrop factor =
+    case factor of
+        3 ->
+            "Pling"
+        5 ->
+            "Plang"
+        7 ->
+            "Plong"
+        _ ->
+            ""
+
+extractDrop : Int -> Int -> String
+extractDrop num factor =
+    if (hasFactor factor num) then
+        correspondingDrop factor
+    else
+        ""
+
+raindrops : Int -> String
+raindrops num =
+    let
+        foundDrops = String.concat (List.map (extractDrop num) factors)
+    in
+        if not (String.isEmpty foundDrops) then
+            foundDrops
+        else
+            toString num
