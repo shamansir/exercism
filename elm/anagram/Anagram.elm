8d88415ed29de943774a576bd5808ddf5c0c8026
Ulric Wilfred <shaman.sir@gmail.com>
2016-09-11 19:28:07 +0300

    solve elm/Anagram

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -1,5 +1,26 @@
 module Anagram exposing (..)
 
+import String
+
+sortChars: String -> String
+sortChars source =
+    source
+        |> String.toLower
+        |> String.toList
+        |> List.sort
+        |> String.fromList
+
+isAnagram: String -> String -> Maybe String
+isAnagram subject sample =
+    if String.toLower subject ==
+       String.toLower sample then
+        Nothing
+    else if sortChars subject ==
+            sortChars sample then
+        Just sample
+    else
+        Nothing
+
 detect : String -> List String -> List String
 detect subject samples =
-    []
+    List.filterMap (isAnagram subject) samples
