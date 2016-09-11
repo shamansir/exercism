2e58c2dc153292db5efff721ef7d4edeba112153
Ulric Wilfred <shaman.sir@gmail.com>
2016-09-11 19:31:26 +0300

    improve anagram/Anagram solution

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -5,21 +5,24 @@ import String
 sortChars: String -> String
 sortChars source =
     source
-        |> String.toLower
         |> String.toList
         |> List.sort
         |> String.fromList
 
 isAnagram: String -> String -> Maybe String
 isAnagram subject sample =
-    if String.toLower subject ==
-       String.toLower sample then
-        Nothing
-    else if sortChars subject ==
-            sortChars sample then
-        Just sample
-    else
-        Nothing
+    let
+        lowerSubject = String.toLower subject
+        lowerSample = String.toLower sample
+    in
+        if String.toLower lowerSubject ==
+           String.toLower lowerSample then
+            Nothing
+        else if sortChars lowerSubject ==
+                sortChars lowerSample then
+            Just sample
+        else
+            Nothing
 
 detect : String -> List String -> List String
 detect subject samples =
