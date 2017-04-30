cc0c212c1e8deee8bed4290fe95379320e1c20a1
Ulric Wilfred <shaman.sir@gmail.com>
2017-04-30 23:35:44 +0200

    initial implementation of the WordCount task

--- elm/word-count/WordCount.elm
+++ elm/word-count/WordCount.elm
@@ -1,4 +1,17 @@
-module WordCount exposing (..)
+module WordCount exposing (wordCount)
 
+import Dict exposing (..)
 
 wordCount : String -> Dict String Int
+wordCount phrase =
+    let
+      words = phrase |> String.words
+      result = Dict.empty
+    in
+      List.foldl addWord result words
+
+addWord : String -> Dict String Int -> Dict String Int
+addWord word dict =
+    case dict |> Dict.get word of
+        Nothing -> dict |> Dict.insert word 1
+        Just count -> dict |> Dict.insert word (count + 1)
